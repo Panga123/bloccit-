bf2232a3735fc480d41848a22402a1309840c5c6
Ann Murray Dunning <ann@amurraypr.com>
2018-07-04 22:10:24 -0700

    Assignment Rails Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  
+
 end
