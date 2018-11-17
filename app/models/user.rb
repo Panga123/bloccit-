bc191a8b77f4bb3aead03348397375e94da6be1e
Ann Murray Dunning <ann@amurraypr.com>
2018-11-17 15:06:25 -0800

    Assignment Rails Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # before_save { self.email = email.downcase }
 
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -17,4 +18,17 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: true }, length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+
+end
+
 end
