4e444cb0d473160afaea448f3daa91d11d949112
Ann Murray Dunning <ann@amurraypr.com>
2018-11-18 19:59:49 -0800

    Assignment Posts and Users Finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
-
-  validates :title, length: { minimum: 5 }, presence: true
+  scope :ordered_by_title, -> { order('title DESC')}
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
