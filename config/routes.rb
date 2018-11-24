01e7a3010902c74f51b3d8ac5d7ba5ce7972c903
Ann Murray Dunning <ann@amurraypr.com>
2018-11-23 19:21:16 -0800

    Checkpoint Rails Comments Finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
 
   resources :topics do
-  resources :posts, except: [:index]
-# #34
-   # resources :posts, except: [:index]
-   resources :posts, only: [] do
-   resources :comments, only: [:create, :destroy]
- end
+    resources :posts, except: [:index]
+    # #34
+    # resources :posts, except: [:index]
 
+  end
 
- # #4
-   resources :posts, only: [] do
- # #5
-     resources :comments, only: [:create, :destroy]
-   end
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
 
-end
+  end
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
@@ -24,7 +19,7 @@ end
   resources :advertisements
 
   # resources :advertisements
- resources :posts
+  resources :posts
 
   get 'about' => 'welcome#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CommentsController, type: :controller do
   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
   let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
-  
+
   # #6
   context "guest" do
     describe "POST create" do
@@ -20,12 +20,12 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     describe "DELETE destroy" do
-      it "redirects the user to the sign in view" do
-        delete :destroy, params: { post_id: my_post.id, id: comment.id }
-        expect(response).to redirect_to(new_session_path)
-      end
-    end
-  end
+       it "redirects the user to the sign in view" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         expect(response).to redirect_to(new_session_path)
+       end
+     end
+end
 
   # #7
   context "member user doing CRUD on a comment they don't own" do
