d401839d8386b2b526c4599f7e9a54aa65088503
Ann Murray Dunning <ann@amurraypr.com>
2018-11-24 21:22:38 -0800

    Checkpoint Rails Voting Finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # before_save { self.email = email.downcase }
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
