5ec41fc7f5fd5d5e0368507c79b301a29da1938c
Ann Murray Dunning <ann@amurraypr.com>
2018-11-25 09:50:45 -0800

    Assignment Rails Voting Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   #4 association with votes
   has_many :votes, dependent: :destroy
 
+# adding callback
+  after_create :create_vote
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -33,5 +35,12 @@ class Post < ApplicationRecord
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+private
+
+#creating new upvote for post
+def create_vote
+  user.votes.create(value: 1, post: self)
+end 
 
 end
