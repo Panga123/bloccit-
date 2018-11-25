5d765be0bc52e7c6392efe548ef030123c488710
Ann Murray Dunning <ann@amurraypr.com>
2018-11-25 09:25:47 -0800

    Checkpoint Rails Voting Done Re-Submit

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,9 @@
-class Vote < ApplicationRecord 
+class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
   after_save :update_post
 
+#this gave me a bit of trouble
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
 
   private
